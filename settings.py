e84e551f598cca67c56dd2b56b67791ae3f43986
Hayden <37120379+haydenwalker980@users.noreply.github.com>
2020-11-21 11:41:29 -0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,13 +2,13 @@ import os
 
 # The prefix that will be used to parse commands.
 # It doesn't have to be a single character!
-COMMAND_PREFIX = "!"
+COMMAND_PREFIX = "ww!"
 
 # The bot token. Keep this secret!
 BOT_TOKEN = "Get this from https://discord.com/developers/applications"
 
 # The now playing game. Set this to anything false-y ("", None) to disable it
-NOW_PLAYING = COMMAND_PREFIX + "commands"
+NOW_PLAYING = "on winOS 4.0.0 | " + COMMAND_PREFIX + "commands"
 
 # Base directory. Feel free to use it if you want.
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
